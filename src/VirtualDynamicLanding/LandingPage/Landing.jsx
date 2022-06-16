e49db49571e161073ed2da1abdeecbba45ed6d24
brandon.liu <brandon.liu@meddataquest.com>
2022-06-16 00:27:20 -0700

    Scratch the team page

--- src/VirtualDynamicLanding/LandingPage/Landing.jsx
+++ src/VirtualDynamicLanding/LandingPage/Landing.jsx
@@ -5,6 +5,7 @@ import IndexHeader from "components/Headers/IndexHeader.js";
 import LandingPageNavbar from "./LandingPageNavbar";
 import LandingPageContent from "./LandingPageContent"
 import LandingPageFooter from "./LandingPageFooter";
+import TeamPageContent from "VirtualDynamicLanding/TeamPage/TeamPageContent";
 /*! Copyright 2022 Virtual Dynamic Labs */
 // nodejs library that concatenates classes
 // ReactJS plugin for a nice carousel
@@ -51,12 +52,13 @@ export default function Landing() {
                 </div>
               </div>
             </div>
-            <img
+            {/* <img
               alt="..."
               src={require("./Images/meta-video.jpeg").default}
               width="100%"
               height="750px"
-            />
+            /> */}
+            <TeamPageContent/>
           </div>
           <LandingPageFooter />
         </div>
