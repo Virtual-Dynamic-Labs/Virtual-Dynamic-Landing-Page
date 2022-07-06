e13fbe4f1c7d48beda6bd33679481d8ccad00d2d
Michael Zhou <michael.zhouzzy@gmail.com>
2022-07-06 13:02:52 -0700

    Fix whitepaper height not fit to window size.

--- src/VirtualDynamicLanding/WhitePaper/WhitePaper.jsx
+++ src/VirtualDynamicLanding/WhitePaper/WhitePaper.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import {useState} from 'react';
 import LandingPageNavbar from "../LandingPage/LandingPageNavbar";
 import background from "../LandingPage/Images/metaverse.jpg";
 
+const getWindowSize = () => {
+    const {innerWidth, innerHeight} = window;
+    return {innerWidth, innerHeight};
+}
+
 const WhitePaper = () => {
+    const [windowSize] = useState(getWindowSize());
+    let googleDocHeight = windowSize.innerHeight - 100;
+    const contentStyle = { paddingTop: "100px", display: "block", textAlign: "center"};
     return (
         <>
         <div style={{
           backgroundImage: `url(${background})`
         }}>
         <LandingPageNavbar />
-            <div className="content" style={{ paddingTop: "100px", display: "block",  textAlign: "center"}}>
-                <iframe src="https://docs.google.com/document/d/e/2PACX-1vS-kIkhtOeANBcCW_LVLNr6TifEzAZP3ASNAeRnM5TXIR29ndaQeHt-XNKmfOCUG5j7qgXlH7RMoPyI/pub?embedded=true" width="800" height="600" frameborder="1px" ></iframe>
+            <div className="content" style={contentStyle}>
+                <iframe title="white_paper_link" src="https://docs.google.com/document/d/e/2PACX-1vS-kIkhtOeANBcCW_LVLNr6TifEzAZP3ASNAeRnM5TXIR29ndaQeHt-XNKmfOCUG5j7qgXlH7RMoPyI/pub?embedded=true" width="800" height={googleDocHeight} frameborder="1px" ></iframe>
             </div>
         </div>
         </>
